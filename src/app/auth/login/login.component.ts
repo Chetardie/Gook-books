e76f37117787165b92bfe92a94094b7d2c231cdb
Chetardie <deydera23@gmail.com>
2018-06-28 12:18:18 +0300

    add formControl validity status message

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { AuthService } from '../auth.service';
+import { patternValidator } from '../../shared/pattern-validator';
 
 @Component({
   selector: 'gook-login',
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   private initLoginForm(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      email: new FormControl(null, [Validators.required, Validators.email,  patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../auth.service';
+import { patternValidator } from '../../shared/pattern-validator';
 
 @Component({
   selector: 'gook-register',
@@ -25,7 +26,7 @@ export class RegisterComponent implements OnInit {
 
   private initRegisterForm(): void {
     this.registerForm = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      email: new FormControl(null, [Validators.required, Validators.email, patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
