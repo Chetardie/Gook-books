dd147bcec2495c976b44f08cd9aa508afa5d7eaa
Chetardie <deydera23@gmail.com>
2018-06-30 12:54:25 +0300

    add quote component

---
+++ src/app/quotes/quotes.model.ts
@@ -0,0 +1,11 @@
+import { IBookAuthor } from "../books/models/IBookAuthor";
+
+export class Quote {
+  public description: string;
+  public author: IBookAuthor;
+
+  constructor(quoteInfo: { description: string, author: IBookAuthor }) {
+    this.description = quoteInfo.description;
+    this.author = quoteInfo.author;
+  }
+}
